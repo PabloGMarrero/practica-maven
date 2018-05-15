57ba23ad1973456e1b461de0510cabd8460d535c
Pablo Marrero <pablo.g.marrero@gmail.com>
2018-05-14 23:01:24 -0300

    Se verifica agregado de usuario a la clase Juego

--- src/domain/Juego.java
+++ src/domain/Juego.java
@@ -1,9 +1,22 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Juego {
 
+	private List<Usuario> usuarios;
+	
+	public Juego() {
+		usuarios = new ArrayList<Usuario>();
+	}
+
 	public Integer cantidadUsuarios() {
-		return 0;
+		return usuarios.size();
+	}
+
+	public void agregarUsuario(Usuario usuario) {
+		usuarios.add(usuario);		
 	}
 
 }
--- tests/domain/JuegoTest.java
+++ tests/domain/JuegoTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,15 +9,23 @@ import org.junit.jupiter.api.Test;
 class JuegoTest {
 
 	private Juego juego;
+	private Usuario usuarioMock;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		juego = new Juego();
+		usuarioMock = mock(Usuario.class);
 	}
 
 	@Test
 	void testCreacionalJuego() {
 		assertTrue(juego.cantidadUsuarios() == 0);
+	}
+	
+	@Test
+	void testAgregoUnUsuarioAlJuego() {
+		juego.agregarUsuario(usuarioMock);
+		assertTrue(juego.cantidadUsuarios() == 1);
 	}
 
 }
